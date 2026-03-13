72b654fb187a3166db5f51d6a34cf0513d30b4c9
LydieForgit <lydie.forgit@oclock.school>
2026-03-13 17:45:23 +0100

    handleColorClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import { pieceColors, generateSecretCode } from './Game/Initiate.js';
 
@@ -8,13 +8,30 @@ function App() {
   const [ check, setCheck ] = useState([]);
   const [ proposals, setProposals ] = useState([]);
   const [ caseClicked, setCaseClicked ] = useState(null);
+  const [ pickedColor, setPickedColor ] = useState(null);
   
-  console.log(secretCode);
+function handleColorClick(pickedColor, caseClicked) {
+  if (caseClicked !== null && pickedColor !== null) {
+    const newProposals = [...proposals];
+    newProposals[turn-1][caseClicked] = pickedColor;
+    setProposals(newProposals);
+    setCaseClicked(null);
+    setPickedColor(null);
+  }
+}
 
   return (
     <>
       <div>
       <h1>Mastermind</h1>
+      </div>
+      <div>
+        {pieceColors.map((color, index) => (
+          <div 
+          key={index} 
+          className={`${color} case`}
+          onClick={()=>setPickedColor(color)}></div>
+        ))}
       </div>
       <div className='gameContainer'>
         {Array.from({ length: 10 }).map((_, rowIndex) => (
